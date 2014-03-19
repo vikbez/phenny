e3b4a4fc70bed25fd412a92c9664acb71e919c53
vikbez <v@42.am>
2014-03-19 01:04:33 +0100

    pep8 reload module + more standard command usage

--- modules/reload.py
+++ modules/reload.py
@@ -10,46 +10,52 @@ http://inamidst.com/phenny/
 import sys, os.path, time, imp
 import irc
 
-def f_reload(phenny, input): 
-   """Reloads a module, for use by admins only.""" 
-   if not input.admin: return
 
-   name = input.group(2)
-   if name == phenny.config.owner: 
-      return phenny.reply('What?')
+def f_reload(phenny, input):
+    """Reloads a module, for use by admins only."""
+    if not input.admin:
+        return
 
-   if (not name) or (name == '*'): 
-      phenny.variables = None
-      phenny.commands = None
-      phenny.setup()
-      return phenny.reply('done')
+    name = input.group(2)
 
-   if not sys.modules.has_key(name): 
-      return phenny.reply('%s: no such module!' % name)
+    if not name:
+        return phenny.reply('reload what ?')
 
-   # Thanks to moot for prodding me on this
-   path = sys.modules[name].__file__
-   if path.endswith('.pyc') or path.endswith('.pyo'): 
-      path = path[:-1]
-   if not os.path.isfile(path): 
-      return phenny.reply('Found %s, but not the source file' % name)
+    if name == phenny.config.owner:
+        return phenny.reply('What?')
 
-   module = imp.load_source(name, path)
-   sys.modules[name] = module
-   if hasattr(module, 'setup'): 
-      module.setup(phenny)
+    if name == '*':
+        phenny.variables = None
+        phenny.commands = None
+        phenny.setup()
+        return phenny.reply('done')
 
-   mtime = os.path.getmtime(module.__file__)
-   modified = time.strftime('%Y-%m-%d %H:%M:%S', time.gmtime(mtime))
+    if not sys.modules.has_key(name):
+        return phenny.reply('%s: no such module!' % name)
 
-   phenny.register(vars(module))
-   phenny.bind_commands()
+    # Thanks to moot for prodding me on this
+    path = sys.modules[name].__file__
+    if path.endswith('.pyc') or path.endswith('.pyo'):
+        path = path[:-1]
+    if not os.path.isfile(path):
+        return phenny.reply('Found %s, but not the source file' % name)
 
-   phenny.reply('%r (version: %s)' % (module, modified))
+    module = imp.load_source(name, path)
+    sys.modules[name] = module
+    if hasattr(module, 'setup'):
+        module.setup(phenny)
+
+    mtime = os.path.getmtime(module.__file__)
+    modified = time.strftime('%Y-%m-%d %H:%M:%S', time.gmtime(mtime))
+
+    phenny.register(vars(module))
+    phenny.bind_commands()
+
+    phenny.reply('%r (version: %s)' % (module, modified))
 f_reload.name = 'reload'
-f_reload.rule = ('$nick', ['reload'], r'(\S+)?')
-f_reload.priority = 'low'
+f_reload.rule = (['reload'], r'(\S+)?')
+f_reload.priority = 'high'
 f_reload.thread = False
 
-if __name__ == '__main__': 
-   print __doc__.strip()
+if __name__ == '__main__':
+    print __doc__.strip()
